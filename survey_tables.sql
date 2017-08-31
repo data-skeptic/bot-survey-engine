ba80b8919ee9898b6e043b3f657e03c8e37c995d
Xiaofei Zheng <xfzhengnankai@gmail.com>
2017-08-30 20:29:50 -0700

    changed/added more columns

--- survey_tables.sql
+++ survey_tables.sql
@@ -9,6 +9,9 @@ DROP TABLE IF EXISTS bot_survey_questions;
 CREATE TABLE bot_survey_questions (
   question_id int not null auto_increment
 , question_text varchar(1024) not null
+, default_next_question_id int 
+, is_starting_question BIT 
+, is_ending_question BIT
 , primary key (question_id)
 );
 
@@ -32,18 +35,18 @@ CREATE TABLE bot_survey_questions (
 -- ;
 -- SELECT * FROM bot_survey_questions;
 
-#bot_survey_responses
+# bot_survey_responses
 # This table should get one record every time a survey is started. It will capture metadata like the time it is started.
 DROP TABLE IF EXISTS bot_survey_responses;
 CREATE TABLE bot_survey_responses(
-survey_id int not null auto_increment
+response_id int not null auto_increment
 ,user_id int 
-,survey_start_time TIMESTAMP not null
-,primary key (survey_id)
+,response_start_time TIMESTAMP not null
+,primary key (response_id)
 );
 
 
-INSERT INTO bot_survey_responses(survey_id, user_id, survey_start_time) 
+INSERT INTO bot_survey_responses(response_id, user_id, survey_start_time) 
 values(3, 1, NOW()),
 (4,13, NOW());
 SELECT * from bot_survey_responses;
@@ -52,16 +55,16 @@ SELECT * from bot_survey_responses;
 # bot_survey_response_answers
 DROP TABLE IF EXISTS bot_survey_response_answers;
 CREATE TABLE bot_survey_response_answers(
-survey_id int not null
+response_id int not null
 ,user_id int
 ,question_id int not null
 ,question_order int 
-,response_time TIMESTAMP not null
-,response_text varchar(1024) not null
-,FOREIGN KEY (survey_id) REFERENCES bot_survey_responses(survey_id)
+,answer_time TIMESTAMP not null
+,answer_text varchar(1024) not null
+,FOREIGN KEY (response_id) REFERENCES bot_survey_responses(response_id)
 );
 
-INSERT INTO bot_survey_response_answers(survey_id, user_id, question_id, question_order, response_time, response_text) 
+INSERT INTO bot_survey_response_answers(response_id, user_id, question_id, question_order, answer_time, answer_text) 
 values(1, 200, 1, 1, NOW(), "In 2015, I started listening to Data Skeptic"),
 (1,200,2,2,Now(), "My academic background is Math");
 SELECT * from bot_survey_response_answers;
@@ -69,13 +72,11 @@ SELECT * from bot_survey_response_answers;
 
 
 # survey_branching_logic 
-# more work to be done.
 CREATE TABLE survey_branching_logic (
 question_id int not null
 ,magic_text varchar(1024) not null
-,action_to_take 
+,next_question_id_if_magic int 
 );
-
 
 show tables;
 
