c4bddd8e546a851e7b9927283f4af0406d347835
Xiaofei Zheng <xfzhengnankai@gmail.com>
2017-09-06 16:02:50 -0700

    survey class

---
+++ survey/survey.py
@@ -0,0 +1,111 @@
+import sqlalchemy
+import pymysql
+pymysql.install_as_MySQLdb()
+from sqlalchemy.orm import sessionmaker
+import datetime
+import time
+from pandas import DataFrame
+
+with open ("mysql_password.txt", "r") as myfile:
+    password=myfile.readlines()[0].strip()    
+
+class survey():   
+    def __init__(self, username, password, address, databasename):
+        #connect to sqlworkbench/J
+        #engine_internal = sqlalchemy.create_engine("mysql://%s:%s@%s/%s" % ("xiaofei", password, "iupdated.com:3306","survey"),pool_size=3, pool_recycle=3600)
+        engine_internal = sqlalchemy.create_engine("mysql://%s:%s@%s/%s" % (username, password, address,databasename),pool_size=3, pool_recycle=3600)
+        Internal = sessionmaker(bind=engine_internal)
+        internal = Internal()
+        self.internal = internal 
+        #test
+        try:
+            internal.execute("SHOW DATABASES;")
+            print('The connection is successful.')
+        except:
+            print('The connection fails.')
+            raise
+        # call method refresh_from_database
+        self.refresh_from_database('bot_survey_questions')
+    def refresh_from_database(self,table_name):
+        try:
+            r = self.internal.execute("SELECT * FROM "+ table_name)
+            print('Refresh '+ table_name +' is successful.')
+            data = r.fetchall()
+            df = DataFrame(data)
+            #df.columns = r.keys()
+            print(df)
+            return df
+        except:
+            print("Error in refresh " + table_name)
+            raise
+    def get_next_question_id(self, question_id, answer):
+        question_df = self.refresh_from_database("logic_branches") #logic_branches is the table name saving the logic branches.
+        sub_df = question_df.loc[(question_df['question_id'] == question_id) 
+                                 & ((question_df['test_text'].isnull()) | (question_df['test_text'].apply(lambda x: str(x).lower() in answer.lower())))]
+        
+        return sub_df['next_question_id'][0] if sub_df['next_question_id'].shape[0] == 1 else print('error')
+        # When we design the table logic_branches, we should take the case that there is no next question into consideration. 
+        # In this case, the next_question_id is -1.
+    
+    # update table bot_survey_response_answers and bot_survey_responses with users' answers.
+    def save_answer(self, response_id, question_id, question_order, answer_text):
+        '''
+        write a function to update responses and answers tables.
+        '''
+        # update table bot_survey_responses if response_id is None
+        if response_id is None:  # a new survey starts when response_id is None.
+            # we insert a new row in table bot_survey_responses to update the response_start_time
+            # response_id will be generated automatically when insert a new row.
+            try: 
+                template = "INSERT INTO bot_survey_responses(response_start_time) VALUES(NOW())"
+                query = template.format()
+                self.internal.execute(query)
+                # retrivel the response_id if the insertation is sucessful.
+                r = self.internal.execute("SELECT last_insert_id()")
+                response_id = r.fetchone()[0]
+                #print(response_id)
+            except: 
+                print("Error")
+                raise
+        # response_id is known for the moment.
+        # check whether the survey is over. If yes, then update the end_time and reset response_id = None
+        last_question_list = [4,5]  # just for test todo: query from question_table to get a list of ending question_ids.
+        if question_id in last_question_list:
+            # Then the survey is over
+            template = "UPDATE bot_survey_responses SET response_end_time = Now() WHERE response_id = '{response_id}';"
+            query = template.format(response_id=response_id)
+            self.internal.execute(query)
+            print('Update the response end time successfully.') 
+
+        # update table bot_survey_response_answers   
+        try: 
+            # response_answer_id will be automatically added by sql when we insert new rows.
+            template = "INSERT INTO bot_survey_response_answers (response_id, question_id, question_order, answer_time, answer_text) VALUES('{response_id}','{question_id}', '{question_order}', NOW(), '{answer_text}')"
+            query = template.format(response_id = response_id, question_id=question_id,  question_order=question_order, answer_text=answer_text)
+            self.internal.execute(query) 
+            # retrivel the response_id if the insertation is sucessful.
+            r = self.internal.execute("SELECT LAST_INSERT_ID()") 
+            response_answer_id = r.fetchone()[0]
+
+            print('Insert into the answers table successfully.')
+            print("Get the response_answer_id successfully.")
+        except: 
+            print('Error in inserting into the answers table.')
+            print('Error in getting response_anwser_id.')
+            raise
+
+        return response_id, response_answer_id
+    def get_magic_reply(self, answer_text, question_id):
+        template = "SELECT * FROM magic WHERE question_id = '{question_id}' "
+        r = self.internal.execute(template.format(question_id = question_id))
+        n= r.rowcount
+
+        rows = []
+        for i in range(n):
+            text_reply = r.fetchone()
+            if text_reply['magic_text'] in answer_text:
+                return text_reply['magic_reply']
+        return ""
+        
+
+    
