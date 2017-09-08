78c614e091d9f8ba47f067146ed7a62580191945
Xiaofei Zheng <xfzhengnankai@gmail.com>
2017-09-08 13:36:11 -0700

    Update api.py

--- survey/api.py
+++ survey/api.py
@@ -75,4 +75,4 @@ if __name__ == '__main__':
     api.add_resource(SaveAnswer,   '/survey/response/answer/save')
     app.run(host='0.0.0.0', debug=False, port=3500)
     logger.info("Ready")
-    print("***")
+    
