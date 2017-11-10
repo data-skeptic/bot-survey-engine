66e925d7d84cf4eb67d1c7d7666ef88ef3a6b05f
Xiaofei Zheng <xfzhengnankai@gmail.com>
2017-11-10 14:52:50 -0800

    add comments

--- GA_project/run_model.py
+++ GA_project/run_model.py
@@ -1,20 +1,28 @@
 from rasa_nlu.converters import load_data
 from rasa_nlu.config import RasaNLUConfig
 from rasa_nlu.model import Trainer
+from rasa_nlu.model import Metadata, Interpreter
 import json
 import os 
 
-
 dir_path = os.path.dirname(os.path.realpath(__file__))
-training_data =  load_data(dir_path +'/data/training_data.json')
-trainer = Trainer(RasaNLUConfig("sample_configs/config_spacy.json"))
-trainer.train(training_data)
-model_directory = trainer.persist('./projects/')  # Returns the directory the model is stored in
+#-----train a new model and save it in mdoel_directory-----------
+# load training data
+# training_data =  load_data(dir_path +'/data/training_data.json')
+# # set config and train
+# trainer = Trainer(RasaNLUConfig("sample_configs/config_spacy.json"))
+# trainer.train(training_data)
+# # Returns the directory the model is stored in 
+# model_directory = trainer.persist('./projects/') 
+#----------------end of training and saving-----------------------
 
-from rasa_nlu.model import Metadata, Interpreter
-# where `model_directory points to the folder the model is persisted in
+#----------------load the model already trained-------------------
+#model_directory stores all information of the model.
+model_directory = dir_path + "/projects/default/model_20171110-144019"
+#where `model_directory points to the folder the model is persisted in
 interpreter = Interpreter.load(model_directory, RasaNLUConfig("sample_configs/config_spacy.json"))
 
+# use the model to parse testing data and save the result in a json file /data/testing_results.json
 file_name = dir_path + "/data/testing_data.txt" 
 result_file_path = dir_path + "/data/testing_results.json"
 result_file = open(result_file_path, 'w')
@@ -34,5 +42,9 @@ with open(file_name, 'r') as f:
         i += 1
     result_file.write("]\n}")
 result_file.close()
+
+# further process of the parsed data to get legal dimensions and metrics in GA.
+
+
 
 
