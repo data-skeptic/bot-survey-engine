b3b007cabe684d422eed745ee50139c1d03d092b
Kyle Polich <kyle@datascience.com>
2017-11-10 10:48:53 -0800

    update test

--- gahelper/test.py
+++ gahelper/test.py
@@ -4,3 +4,10 @@ from gahelper import Gahelper
 config = json.load(open("../config/config.json"))
 
 ga = Gahelper(config)
+metrics = ['ga:newUsers']
+dimensions = ['ga:medium']
+start_date = '2017-11-01'
+end_date = '2017-11-10'
+report = ga.get_report(metrics, dimensions, start_date, end_date)
+
+print(report)
