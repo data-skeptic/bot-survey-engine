59d153972cbfda0592d9e66b18e0eb5df0b1b85b
Xiaofei Zheng <xfzhengnankai@gmail.com>
2017-11-14 16:28:21 -0800

    comment episodes when testing ga

--- api.py
+++ api.py
@@ -113,40 +113,40 @@ class SaveAnswer(Resource):
         return resp
 
 # episode
-start = time.time()
-print("*************episode session*************")
-print('Downloading word_vec from AWS S3...')
-load_word_vec_instance = load_word_vec()
-update_episode = False
-episode_instance = episode(update_episode,username, address,password,databasename)
-class give_recommendation(Resource):
-    def post(self):
-        r = request.get_data()
-        req = json.loads(r.decode('utf-8'))
-        user_request = req['request']
-        start = time.time()
-        result = episode_instance.recommend_episode(user_request)
-        print("the time it takes to make a recommendation is ", time.time() - start)
-        # start = time.time()
-        # episode_instance.save_recommendation_table(user_request, result)
-        # print('the time it takes to save the recommendation to table is ', time.time() - start)
-        if len(result) > 0:
-            return result
-        else:
-            return None
+# start = time.time()
+# print("*************episode session*************")
+# print('Downloading word_vec from AWS S3...')
+# load_word_vec_instance = load_word_vec()
+# update_episode = False
+# episode_instance = episode(update_episode,username, address,password,databasename)
+# class give_recommendation(Resource):
+#     def post(self):
+#         r = request.get_data()
+#         req = json.loads(r.decode('utf-8'))
+#         user_request = req['request']
+#         start = time.time()
+#         result = episode_instance.recommend_episode(user_request)
+#         print("the time it takes to make a recommendation is ", time.time() - start)
+#         # start = time.time()
+#         # episode_instance.save_recommendation_table(user_request, result)
+#         # print('the time it takes to save the recommendation to table is ', time.time() - start)
+#         if len(result) > 0:
+#             return result
+#         else:
+#             return None
 
-print("How long does it spend in the episode session ", time.time() - start)
+# print("How long does it spend in the episode session ", time.time() - start)
 
-class save_recommendation(Resource):
-    def post(self):
-        r = request.get_data()
-        info = json.loads(r.decode('utf-8'))
-        print('info is ', info)
-        user_request = info.get('user_request')
-        recommendation = info.get('recommendation')
-        start = time.time()
-        episode_instance.save_recommendation_table(user_request,recommendation)
-        print('the time it takes to save the recommendation to table is ', time.time() - start)
+# class save_recommendation(Resource):
+#     def post(self):
+#         r = request.get_data()
+#         info = json.loads(r.decode('utf-8'))
+#         print('info is ', info)
+#         user_request = info.get('user_request')
+#         recommendation = info.get('recommendation')
+#         start = time.time()
+#         episode_instance.save_recommendation_table(user_request,recommendation)
+#         print('the time it takes to save the recommendation to table is ', time.time() - start)
 
 #listener_reminder
 print("*************listener reminder session*************")
@@ -191,8 +191,8 @@ if __name__ == '__main__':
     api.add_resource(GetQuestion,  '/survey/question/<int:question_id>')
     api.add_resource(SaveAnswer,   '/survey/response/answer/save')
     # episode
-    api.add_resource(give_recommendation,   '/episode/recommendation')
-    api.add_resource(save_recommendation,   '/episode/save_recommendation')
+    # api.add_resource(give_recommendation,   '/episode/recommendation')
+    # api.add_resource(save_recommendation,   '/episode/save_recommendation')
     # listener_reminder
     api.add_resource(reminder,  '/listener_reminder')
     
